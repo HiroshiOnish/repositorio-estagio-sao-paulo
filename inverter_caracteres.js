36655b05184d2d00e24c3ce06b04a8889fb31210
Hiroshi Onishi <108165775+HiroshiOnish@users.noreply.github.com>
2024-09-28 18:02:49 -0300

    resposta 5

---
+++ inverter_caracteres.js
@@ -0,0 +1,19 @@
+
+function inverterString(str) {
+    let stringInvertida = '';
+
+    
+    for (let i = str.length - 1; i >= 0; i--) {
+        stringInvertida += str[i];
+    }
+
+    return stringInvertida;
+}
+
+
+let stringOriginal = "Exemplo de string";
+
+
+let resultado = inverterString(stringOriginal);
+console.log("String original:", stringOriginal);
+console.log("String invertida:", resultado);
